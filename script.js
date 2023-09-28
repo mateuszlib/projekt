c24200b408b20c301b3581c1a091c17fd7ed9abc
mateuszlib <146341254+mateuszlib@users.noreply.github.com>
2023-09-28 13:26:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var a =5 ;
+var b=10;
+
+var suma=a+b;
+var roznica=a-b;
+var iloczyn=a*b;
+var iloraz=a/b
+
+
+document.write(suma,"...");
+document.write(roznica,"...");
+document.write(iloczyn,"...");
+document.write(iloraz,"...","<br>");
+
+
+var liczba=5;
+var tekst="random";
+var ulamek=0.6;
+var logiczna=true;
+document.write(liczba,"...",tekst,"...",ulamek,"...",logiczna,"...","<br>");
+
+document.write("<ul>","<li>",liczba,"</li>","<li>",tekst,"</li>","<li>",ulamek,"</li>","<li>",logiczna,"</li>","</ul>","<br>")
+
+
+let x = 6;
+let z = Math.pow(x,2);
+document.write(z,"<br>");
+
+
+//var dziesietny=13;
+//var szesnastkowy=0xACB;
+//var osemkowy=037;
+//var notacjawykladnicza=0.12e-2;
+//var dwojkowy=0101;
+
+
+var mn1=4;
+var mn2=5
+var w1=mn1*mn2;
+var w2=w1/mn2;
+
+
+
+document.write("<h3>",'Mnozenie:',mn1,"+",mn2,"=",w1,"</h3>","<br>",
+"<h3>",'Dzielenie:',w1,"/",mn2,"=",w2,"</h3>",)
+
+
+
